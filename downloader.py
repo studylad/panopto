1f4d33fbecd6bf0cd495389ada83d72e23fbe743
Sourcery AI <>
2022-03-22 12:07:39 +0000

    'Refactored by Sourcery'

--- downloader.py
+++ downloader.py
@@ -35,7 +35,7 @@ def usage():
 
 
 def valid_name(filename):
-    valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+    valid_chars = f"-_.() {string.ascii_letters}{string.digits}"
     return ''.join(c for c in filename if c in valid_chars)
 
 
